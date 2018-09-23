e5baf9ece56398bdc1bf966343406042eb2081a7
yukito ohira <yukito.sys.so@gmail.com>
2018-09-23 16:37:31 +0900

    一旦動くようにした spaで起動

--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    <HelloWorld></HelloWorld>
+    <no-ssr>
+      <v-ons-page>
+        <v-ons-toolbar>
+          <div class="center">Title</div>
+        </v-ons-toolbar>
+
+      </v-ons-page>
+    </no-ssr>
   </div>
 </template>
 
 <script>
   import HelloWorld from '@/components/HelloWorld.vue'
+  // Webpack CSS import
+  import 'onsenui/css/onsenui.css';
+  import 'onsenui/css/onsen-css-components.css';
+
+  // JS import
+  import Vue from 'vue';
+  import VueOnsen from 'vue-onsenui'; // This already imports 'onsenui'
+
+  Vue.use(VueOnsen);
+
 
   export default {
     name: "index",
