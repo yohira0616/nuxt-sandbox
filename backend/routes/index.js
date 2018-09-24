499eff6c5de0706add4a7c5e9eba8b28bc2e04e9
yukito ohira <yukito.sys.so@gmail.com>
2018-09-24 11:57:53 +0900

    とりあえずmysqlにデータをinsertできるようにした

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  const Sequelize = require('sequelize');
+  const sequelize = new Sequelize('test_database', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+
+    // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+    operatorsAliases: false
+  });
+
+  const Task = sequelize.define('tasks', {
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+    name: Sequelize.STRING,
+    description: Sequelize.STRING
+  }, {
+    timestamps: false
+  });
+
+  sequelize.sync()
+    .then(() => Task.create({
+      name: 'test',
+      description: 'test_description'
+    }))
+    .then(task => {
+      console.log(task.toJSON());
+    });
+
+  res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
