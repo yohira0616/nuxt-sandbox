9d2ce51571d114f92eed2e6f91a7712fe008ea39
yukito ohira <yukito.sys.so@gmail.com>
2019-01-12 16:41:03 +0900

    layout

---
+++ layouts/single.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span>single layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt/>
+    <nuxt-link to="/">トップへ戻る</nuxt-link>
+    <hr>
+    <footer>
+      footer
+    </footer>
+  </div>
+</template>
+<script>
+  import AppNavigation from '~/components/AppNavigation.vue'
+
+  export default {
+    components:{
+      AppNavigation
+    }
+  }
+
+</script>
