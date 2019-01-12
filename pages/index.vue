7cff989451e760333af0c5beca7e1af2f7344849
yukito ohira <yukito.sys.so@gmail.com>
2019-01-12 15:04:01 +0900

    nuxt-link

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,12 @@
         <li v-for="item in items" :key="item.id">
           <h4>
             <span>{{item.title}}</span>
-            <small> by {{item.user.id}}</small>
+            <small>
+              by
+              <nuxt-link :to="`/users/${item.user.id}`">
+                {{item.user.id}}
+              </nuxt-link>
+            </small>
           </h4>
           <div>{{item.body.slice(0,130)}}....</div>
           <p><a :href="item.url">{{item.url}}</a></p>
