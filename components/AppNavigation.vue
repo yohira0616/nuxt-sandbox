4884bdf8dd8edc5af0cbe80bb5a234b7c56904da
yukito ohira <yukito.sys.so@gmail.com>
2019-01-12 16:38:34 +0900

    layout child

---
+++ components/AppNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li><nuxt-link to="/">home</nuxt-link></li>
+    <li><nuxt-link to="/child">child</nuxt-link></li>
+  </ul>
+    
+</template>
+
+<script>
+  export default {
+    name: "AppNavigation"
+  }
+</script>
+
+<style scoped>
+
+</style>
